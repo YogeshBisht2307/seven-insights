186c0cbe69a8c146d5c7157d2cb74502b5ef97e2
Yogesh Bisht <yogesh.bisht@iotfy.co>
2023-10-16 00:48:34 +0530

    application build related changes and fixes

--- src/app/admin/posts/add-post/page.tsx
+++ src/app/admin/posts/add-post/page.tsx
@@ -37,7 +37,7 @@ export default async function AddPost() {
                         <svg className="w-3.5 h-3.5 mr-2 text-primary flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                         </svg>
-                        Write a strong headline that accurately reflects the content of the article and grabs the reader's attention.
+                        Write a strong headline that accurately reflects the content of the article and grabs the reader&apos;s attention.
                     </li>
                 </ul>
             </div>
