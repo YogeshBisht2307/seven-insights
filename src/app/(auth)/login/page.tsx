d3d17ef753bcf794fe2f5db1799c343117825ba7
Yogesh Bisht <yogesh.bisht@iotfy.co>
2023-11-24 01:38:26 +0530

    minor changes and fixes in login ui

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -16,8 +16,8 @@ export default async function Login() {
     }
 
     return (
-        <div className="max-w-10xl h-screen sm:pt-32">
-            <div className="w-full max-w-xs p-4 border rounded-lg shadow-sm sm:p-6 md:p-8 mx-auto">
+        <div className="max-w-10xl h-screen flex justify-center items-center">
+            <div className="w-full max-w-xs sm:max-w-sm py-12 px-4 sm:py-8 sm:px-8 border rounded-lg shadow-sm mx-auto">
                 <div className="w-3xl mx-auto">
                     <h1 className="text-xl mb-8 font-bold">Sign In with Credentials</h1>
                 </div>
